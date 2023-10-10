cce36ef3b480eac7e2b02bde1b51edcc3821bfc0
Anderson Sanches <andycds@gmail.com>
2023-10-09 21:21:41 -0300

    first commit

---
+++ Jogo.java
@@ -0,0 +1,27 @@
+public class Jogo {
+    public static void main(String[] args) throws InterruptedException {
+        Personagem cacador = new Personagem("John", 10, 0, 0);
+        Personagem soneca = new Personagem("Soneca", 2, 6, 4);
+        Personagem mineiro = new Personagem("Cruzeirense", 8, 0, 5);
+
+        while (true) {
+            cacador.cacar();
+            soneca.dormir();
+            mineiro.comer();
+            cacador.comer();
+            soneca.dormir();
+            mineiro.cacar();
+            cacador.dormir();
+            soneca.dormir();
+            mineiro.dormir();
+            cacador.cacar();
+            soneca.comer();
+            mineiro.dormir();
+            cacador.cacar();
+            soneca.cacar();
+            mineiro.comer();
+            System.out.println("===========================");
+            Thread.sleep(3000);
+        }
+    }
+}
